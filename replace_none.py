a6a1ce692e671faf498eac6aa1c223491220da7c
abhishekk3 <56911860+abhishekk3@users.noreply.github.com>
2021-02-07 23:45:28 +0800

    Create replace_none.py

---
+++ replace_none.py
@@ -0,0 +1,22 @@
+#Replace None with next element from List. For eg.
+#[None] ==> [None]
+#[None,1,2] ==> [1,1,2]
+#[1,None,2] ==> [1,2,2]
+#[1,4,None,None,3]===> [1,4,3,3,3]
+
+
+def replace_none(arr):
+    
+    for i in range(len(arr)):
+        if arr[i] == None:
+            for j in range(i+1,len(arr)):
+                if j is not None and i+1 < len(arr):
+                    arr.insert(i,arr[j])
+                    arr.pop(j)
+                    break
+    
+    print(arr)
+    
+
+a=[4,5,None,1,2,None,3]
+replace_none(a)
